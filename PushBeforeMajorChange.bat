19e1e35cd8dd94adddc088d56beca3304cf70714
于仲源 <yzhongyuanfff@gmail.com>
2021-06-16 11:46:40 +0200

    Push Before Major Change: structure change, halfedge extraction may be damaged

--- PushBeforeMajorChange.bat
+++ PushBeforeMajorChange.bat
@@ -2,7 +2,7 @@
 git pull origin master
 
 git add .
-git commit -m "Push Before Major Change"
+git commit -m "Push Before Major Change: structure change, halfedge extraction may be damaged"
 git push origin master 
 
 pause
